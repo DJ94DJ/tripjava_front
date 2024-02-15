ec1c7ea9336d58fe50cd5473ded0df30fc9cdd91
nayounghye <yhna.dev@gmail.com>
2024-02-16 05:45:23 +0900

    Feat: map api 공부중..

--- tripjava/src/components/Map/GoogleMapComponent.js
+++ tripjava/src/components/Map/GoogleMapComponent.js
@@ -1,21 +1,51 @@
 import React, { useCallback } from 'react';
-import {
-  APIProvider,
-  Map,
-  useMapsLibrary,
-  useMap,
-} from '@vis.gl/react-google-maps';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 function GoogleMapComponent() {
-  const position = { lat: 61.2176, lng: -149.8997 };
+  const center = {
+    lat: 37.5511694,
+    lng: 126.9882266,
+  };
+
+  const options = {
+    zoom: 16,
+    mapTypeId: 'terrain',
+  };
+
+  // 지도를 불러오는 함수
+  // isLoaded, loadError를 return 한다.
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    // google maps 에서 받은 api key를 전달한다.
+    googleMapsApiKey: process.env.REACT_APP_API,
+  });
+
+  // 지도를 그릴때 동작하는 함수
+  // center 값이나 옵션이 리렌더링에 영향을 받지 않게 하기 위해 useCallback으로 감싸준다.
+  // google map의 instance를 사용할 수 있다.
+  const onLoad = useCallback((map) => {
+    console.log('use map instance', map);
+    map.setCenter(center);
+    map.setOptions(options);
+    map.setHeading(90);
+  }, []);
 
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <APIProvider apiKey={process.env.GOOGLE_MAPS_API_KEY}>
-        <Map center={position} zoom={10}></Map>
-      </APIProvider>
-    </div>
+    isLoaded && (
+      <GoogleMap
+        id="google-map-test"
+        mapContainerStyle={GoogleMapStyle} // width와 height 는 반드시 지정해줘야 한다.
+        onLoad={onLoad}
+      >
+        <Marker />
+      </GoogleMap>
+    )
   );
 }
 
-export default GoogleMapComponent;
+export default React.memo(GoogleMapComponent);
+
+const GoogleMapStyle = {
+  height: '100vh',
+  width: '100%',
+};
