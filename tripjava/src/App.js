83e608cb5f7f1e794c2317bedb51471ae07d052f
nayounghye <yhna.dev@gmail.com>
2024-02-15 18:36:55 +0900

    update : 유저 파일틀 구성

--- tripjava/src/App.js
+++ tripjava/src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import Header from './components/Layout/Header';
 import Main from './pages/Main/Main';
+import UserLoginPage from './pages/User/UserLoginPage';
+import UserSignupPage from './pages/User/UserSignupPage';
+import UserMyPage from './pages/User/UserMyPage';
+import UserFindPage from './pages/User/UserFindPage';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
-      <Main></Main>
+      <Main>
+        <Routes>
+          {/* 로그인/마이페이지 관련 */}
+          <Route path="/login" element={<UserLoginPage />}></Route>
+          <Route path="/signup" element={<UserSignupPage />}></Route>
+          <Route path="/mypage" element={<UserMyPage />}></Route>
+          <Route path="/find" element={<UserFindPage />}></Route>
+          {/* 지도 게시판 */}
+        </Routes>
+      </Main>
     </>
   );
 }
---
+++ tripjava/src/pages/User/UserSignupPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function UserSignupPage() {
+  return <div>UserSignupPage</div>;
+}
+
+export default UserSignupPage;
