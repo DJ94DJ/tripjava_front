840fc215059320e656f71b59626e910fbdb10931
nayounghye <yhna.dev@gmail.com>
2024-02-21 02:58:30 +0900

    Fix: store 수정(redux 실행안되는 부분 해결)

--- tripjava/src/store/store.js
+++ tripjava/src/store/store.js
@@ -1,13 +1,13 @@
 // store.js
-import { createStore, combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './reducers/auth';
 import regiondateReducer from './reducers/regiondateReducer';
 
-const rootReducer = combineReducers({
-  auth: authReducer,
-  region: regiondateReducer,
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    region: regiondateReducer,
+  },
 });
-
-const store = createStore(rootReducer);
 
 export default store;
