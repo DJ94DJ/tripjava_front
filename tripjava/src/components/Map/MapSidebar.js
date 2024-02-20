c76b266e890d38a95aafac9f2972ce0d3fe2580d
nayounghye <yhna.dev@gmail.com>
2024-02-21 06:02:17 +0900

    Feat: 관광API 불러오기 진행중

--- tripjava/src/components/Map/MapSidebar.js
+++ tripjava/src/components/Map/MapSidebar.js
@@ -1,32 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import '../../styles/style.scss';
 
 const MapSidebar = () => {
   const navigate = useNavigate();
   navigate('/');
+  const [touristSpots, setTouristSpots] = useState([]);
+  const selectedRegionName = useSelector(
+    (state) => state.maininfo?.selectedRegionName
+  );
+  // Redux 상태에서 지역명 가져오기
 
-  // api 불러오던거 작업중이었는데 일단 주석처리해둠!
-  // const [touristSpots, setTouristSpots] = useState([]); // 관광지 데이터를 저장할 상태
+  useEffect(() => {
+    // 데이터 로드 함수 정의
+    const fetchTouristSpots = async () => {
+      if (selectedRegionName) {
+        // selectedRegionName이 있을 때만 API 호출
+        try {
+          const response = await fetch(
+            `http://localhost:8080/destination?addr1=${selectedRegionName}`
+          );
+          if (!response.ok) {
+            throw new Error('데이터를 불러오는 데 실패했습니다.');
+          }
+          const data = await response.json();
+          setTouristSpots(data); // 가져온 데이터를 상태에 저장
+        } catch (error) {
+          console.error('Error fetching data: ', error);
+        }
+      }
+    };
 
-  // useEffect(() => {
-  //   // 데이터 로드 함수 정의
-  //   const fetchTouristSpots = async () => {
-  //     try {
-  //       const response = await fetch('http://localhost:8080/api');
-  //       if (!response.ok) {
-  //         throw new Error('데이터를 불러오는 데 실패했습니다.');
-  //       }
-  //       const data = await response.json();
-  //       setTouristSpots(data); // 가져온 데이터를 상태에 저장
-  //     } catch (error) {
-  //       console.error('Error fetching data: ', error);
-  //     }
-  //   };
-
-  //   // 컴포넌트가 마운트될 때 데이터 로드
-  //   fetchTouristSpots();
-  // }, []); // 빈 의존성 배열을 전달하여 컴포넌트가 마운트될 때만 함수가 호출되도록 함
+    fetchTouristSpots();
+  }, [selectedRegionName]); // selectedRegionName이 변경될 때마다 함수를 다시 호출
 
   return (
     <div className="side_menu">
@@ -40,7 +47,17 @@ const MapSidebar = () => {
           />
         </div>
         <div className="sidebar_date">세은님이 보낸 날짜 받깅</div>
-        <div className="sidebar_tourismApi"></div>
+        <div className="sidebar_tourismApi">
+          {touristSpots.map((spot) => (
+            <div key={spot.contentid} className="tourist-spot">
+              <h3>{spot.title}</h3>
+              <p>{spot.addr1}</p>
+              <p>
+                위도: {spot.mapx}, 경도: {spot.mapy}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
