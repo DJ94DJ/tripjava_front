c7071e2ea8a2bb8fdd0679d47f7921cdb8d984d4
nayounghye <yhna.dev@gmail.com>
2024-02-22 20:58:17 +0900

    Fix: Planner 컴포넌트 이름변경취소, 엔터키 추가

--- tripjava/src/components/Planner/PlannerBlock.js
+++ tripjava/src/components/Planner/PlannerBlock.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const PlannerBlock = () => {
   const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState("");
+  const [newItem, setNewItem] = useState('');
 
   const addItem = () => {
-    if(newItem.trim() === "") {
+    if (newItem.trim() === '') {
       return;
     }
     const updatedItems = [...items, { text: newItem, checked: false }];
     setItems(updatedItems);
-    setNewItem("");
+    setNewItem('');
     fetch('http://localhost:8080/planner', {
       method: 'POST',
       headers: {
@@ -44,29 +44,38 @@ const PlannerBlock = () => {
 
   return (
     <div className="test_PlannerBlock">
-    <div className="content">
-    <div className="planner_title">준비물 체크리스트 ✅</div>
-    <hr />
-    <div className="item-notice">* 준비물 이름을 더블클릭하면 삭제됩니다.</div>
-      {items.map((item, index) => (
-        <div key={index}>
-          <input
-            type="checkbox"
-            checked={item.checked}
-            onChange={() => toggleItem(index)}
-          />
-          <span onDoubleClick={() => deleteItem(index)} className="item-text">{item.text}</span>
+      <div className="content">
+        <div className="planner_title">준비물 체크리스트 ✅</div>
+        <hr />
+        <div className="item-notice">
+          * 준비물 이름을 더블클릭하면 삭제됩니다.
         </div>
-      ))}
+        {items.map((item, index) => (
+          <div key={index}>
+            <input
+              type="checkbox"
+              checked={item.checked}
+              onChange={() => toggleItem(index)}
+            />
+            <span onDoubleClick={() => deleteItem(index)} className="item-text">
+              {item.text}
+            </span>
+          </div>
+        ))}
       </div>
       <div className="input-area">
-      <input
-        type="text"
-        value={newItem}
-        onChange={(e) => setNewItem(e.target.value)}
-      />
-      <button onClick={addItem}>추가하기</button>
-    </div>
+        <input
+          type="text"
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              addItem();
+            }
+          }}
+        />
+        <button onClick={addItem}>추가하기</button>
+      </div>
     </div>
   );
 };
