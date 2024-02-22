09b2793c501a68bc73b710266b770cd3a1a7373b
nayounghye <yhna.dev@gmail.com>
2024-02-23 03:28:19 +0900

    Update: MapNearby 파일 생성

---
+++ tripjava/src/components/Map/MapNearby.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const MapNearby = () => {
+  return (
+    <div className="nearby_menu">
+      <div className="nearby_content">
+        <div className="nearby_header"></div>
+        <div className="nearby_hotel">
+          <h3>숙소</h3>
+          <ul></ul>
+        </div>
+        <div className="nearby_route day1">
+          <ul></ul>
+        </div>
+        <div className="nearby_route day2"></div>
+        <div className="nearby_route day3"></div>
+        <div className="nearby_route day4"></div>
+        <div className="nearby_tourismApi"></div>
+      </div>
+    </div>
+  );
+};
