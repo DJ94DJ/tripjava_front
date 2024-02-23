1f3cd9cbde82ea4a61cfd1ab0ea074d8b886632c
nayounghye <yhna.dev@gmail.com>
2024-02-24 01:50:36 +0900

    design: nearby 프로토 디자인 추가

--- tripjava/src/components/Map/MapNearby.js
+++ tripjava/src/components/Map/MapNearby.js
@@ -4,18 +4,10 @@ export const MapNearby = () => {
   return (
     <div className="nearby_menu">
       <div className="nearby_content">
-        <div className="nearby_header"></div>
-        <div className="nearby_hotel">
-          <h3>숙소</h3>
-          <ul></ul>
+        <div className="nearby_header">
+          <h3>추천 장소</h3>
         </div>
-        <div className="nearby_route day1">
-          <ul></ul>
-        </div>
-        <div className="nearby_route day2"></div>
-        <div className="nearby_route day3"></div>
-        <div className="nearby_route day4"></div>
-        <div className="nearby_tourismApi"></div>
+        <div className="nearby_container"></div>
       </div>
     </div>
   );
