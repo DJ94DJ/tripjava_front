b8d3fb7ac52034ea4994475b55f1224f5478e1eb
nayounghye <yhna.dev@gmail.com>
2024-02-24 14:55:59 +0900

    Feat: nearby 데이터 표시 완료

--- tripjava/src/components/Map/MapNearby.js
+++ tripjava/src/components/Map/MapNearby.js
@@ -10,12 +10,12 @@ import { useSelector } from 'react-redux';
 export const MapNearby = () => {
   return (
     <div className="nearby_menu">
-      <div className="nearby_content">
+      {/* <div className="nearby_content">
         <div className="nearby_header">
           <h3>추천 장소</h3>
         </div>
         <div className="nearby_container"></div>
-      </div>
+      </div> */}
     </div>
   );
 };
